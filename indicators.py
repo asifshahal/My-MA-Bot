d6a6c881f5f48d6bb12f1c4d5b37bfe3d661c127
Mohamed Asif Shahal S <150642131+asifshahal@users.noreply.github.com>
2026-01-11 23:31:39 +0530

    Refactor last_ma_breakouts to last_ma_breakout

--- indicators.py
+++ indicators.py
@@ -1,32 +1,11 @@
-from datetime import datetime
+def last_ma_breakout(df, period):
+    df[f"ma{period}"] = df["close"].rolling(period).mean()
 
-def last_ma_breakouts(df):
-    df["ma50"] = df["close"].rolling(50).mean()
-    df["ma200"] = df["close"].rolling(200).mean()
-    df = df.dropna()
-
-    last_50 = None
-    last_200 = None
-
-    for i in range(len(df) - 1, 0, -1):
+    for i in range(len(df) - 1, period, -1):
         prev = df.iloc[i - 1]
         curr = df.iloc[i]
 
-        # MA50 breakout
-        if last_50 is None:
-            if prev.close < prev.ma50 and curr.close > curr.ma50:
-                last_50 = ("ABOVE", datetime.fromtimestamp(curr.time / 1000))
-            elif prev.close > prev.ma50 and curr.close < curr.ma50:
-                last_50 = ("BELOW", datetime.fromtimestamp(curr.time / 1000))
+        if prev["close"] < prev[f"ma{period}"] and curr["close"] > curr[f"ma{period}"]:
+            return curr["date"].date()
 
-        # MA200 breakout
-        if last_200 is None:
-            if prev.close < prev.ma200 and curr.close > curr.ma200:
-                last_200 = ("ABOVE", datetime.fromtimestamp(curr.time / 1000))
-            elif prev.close > prev.ma200 and curr.close < curr.ma200:
-                last_200 = ("BELOW", datetime.fromtimestamp(curr.time / 1000))
-
-        if last_50 and last_200:
-            break
-
-    return last_50, last_200
+    return None
